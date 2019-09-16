9f885c30d74573a296e8cc7815dd9990f6c81d13
pavansw1 <pavansatishwankhade@gmail.com>
2019-09-17 04:24:02 +0530

    changes from machine2 from local fork by pavansw1

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 print ("Working on Git Remotely")
 print ("pavansw1 Working on Git Remotely on machine2")
-print ("pavansw Working on Git Remotely on machine1 again")
+print ("pavansw1 Working on Git Remotely on machine2 again")
 
