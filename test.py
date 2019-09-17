5b9f781473345794e697d900e3c1a3fdb7ffed8f
pavansw <pavanwankhade4u@gmail.com>
2019-09-17 16:44:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ print ("Working on Git Remotely")
 print ("pavansw1 Working on Git Remotely on machine2")
 print ("pavansw1 Working on Git Remotely on machine2 again")
 print ("New line from machine2 pavansw1 new branch")
-
+print ("test git to jenkins")
